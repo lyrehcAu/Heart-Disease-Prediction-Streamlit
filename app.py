871a85f2ff693e37af4dd5a06a150902a857de19
lyrehcAu <aurl.cheryl@gmail.com>
2025-09-10 23:48:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load model
+# Model
 model = joblib.load("model/HeartDisease_rf.pkl")  # simpan di folder 'model'
 
 st.title("Heart Disease Prediction App ❤️")
 
-# Input user
+# Input
 age = st.number_input("Age", min_value=1, max_value=120, value=25)
 gender = st.selectbox("Gender", ["male", "female"])
 resting_ecg = st.selectbox("Resting ECG", [0, 1, 2])
@@ -20,7 +20,7 @@ exercise_angina = st.selectbox("Exercise Angina", ["no", "yes"])
 st_depression = st.number_input("ST Depression", min_value=0.0, max_value=10.0, value=1.0, step=0.1)
 st_slope = st.selectbox("ST Slope", ["normal", "upsloping", "flat", "downsloping"])
 
-# Convert categorical to numeric
+# Konversi
 gender = 1 if gender == "male" else 0
 chest_pain_dict = {"typical": 1, "atypical_angina": 2, "non_anginal": 3, "asymptomatic": 4}
 chest_pain = chest_pain_dict[chest_pain]
@@ -29,16 +29,17 @@ exercise_angina = 1 if exercise_angina == "yes" else 0
 slope_dict = {"normal": 0, "upsloping": 1, "flat": 2, "downsloping": 3}
 st_slope = slope_dict[st_slope]
 
-# Prepare data
+# Data
 new_data = np.array([
     age, gender, chest_pain, blood_pressure, cholesterol, fasting_sugar,
     resting_ecg, max_heart_rate, exercise_angina, st_depression, st_slope
 ]).reshape(1, -1)
 
-# Predict button
+# Predict
 if st.button("Predict"):
     prediction = model.predict(new_data)[0]
     if prediction == 0:
-        st.success("✅ No heart disease detected.")
+        st.success("No heart disease detected 🥳")
     else:
-        st.error("⚠️ Heart disease detected.")
+        st.error("Heart disease detected 😟")
+
